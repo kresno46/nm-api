1da8bff2d74a1c13f662e610a8b9929060667de7
VaxXinE <arya312219@gmail.com>
2025-08-01 18:07:55 +0700

    perbaikan error datatype

--- models/news.js
+++ models/news.js
@@ -1,12 +1,12 @@
 module.exports = (sequelize, DataTypes) => {
   const News = sequelize.define('News', {
-    title: { type: DataTypes.TEXT, allowNull: false },
+    title: { type: DataTypes.STRING(255), allowNull: false },
     link: { type: DataTypes.STRING(512), allowNull: false, unique: true },
-    image: { type: DataTypes.TEXT },
+    image: { type: DataTypes.STRING(500) },
     category: { type: DataTypes.STRING },
     date: { type: DataTypes.STRING },
-    summary: { type: DataTypes.TEXT },
-    detail: { type: DataTypes.TEXT },
+    summary: { type: DataTypes.STRING },
+    detail: { type: DataTypes.STRING },
     language: { type: DataTypes.STRING, allowNull: false },
   }, {
     tableName: 'news',
