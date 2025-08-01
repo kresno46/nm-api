6ef2aef8cdd77a1247437aab6d9fbca3c073eb83
VaxXinE <arya312219@gmail.com>
2025-08-01 16:45:03 +0700

    perbaikan kode DB

--- models/index.js
+++ models/index.js
@@ -1,9 +1,17 @@
-const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  dialect: 'mysql', // atau 'postgres', tergantung
-  logging: false,
-});
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(
+  process.env.MYSQLDATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQLPASSWORD,
+  {
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT || 3306,
+    dialect: 'mysql',
+    logging: false
+  }
+);
+
 
 const NewsModel = require('./news');
 const HistoricalDataModel = require('./historical');
